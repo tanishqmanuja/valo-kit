1a44678b8ffc1881dfa679cd2f789c4a01f42ca8
tanishqmanuja <tanishqmanuja@gmail.com>
2023-01-27 13:41:16 +0530

    misc(vry): verbose dev mode banner

--- vry-js/src/helpers/banner.ts
+++ vry-js/src/helpers/banner.ts
@@ -1,15 +1,27 @@
 import chalk from "chalk";
 import { rawLogger } from "../logger/logger.js";
+import { isDevelopment } from "../utils/env.js";
 import { version as vr } from "../utils/version.js";
 
 export const logStartingBanner = () => {
 	rawLogger.info("");
 	rawLogger.info("********************************************************");
 	rawLogger.info(`***************** vRY JS v${vr} Started ****************`);
+
+	if (isDevelopment()) {
+		rawLogger.info(`********************** DEVELOPMENT *********************`);
+	}
+
 	rawLogger.info("********************************************************");
 	rawLogger.info("");
 };
 
 export const printStartingBanner = () => {
-	console.log("⚡️" + chalk.yellow(`VALORANT Rank Yoinker JS v${vr}`), "\n");
+	console.log(
+		"⚡️" +
+			chalk.yellow(
+				`VALORANT Rank Yoinker JS v${vr}${isDevelopment() ? "-Dev" : ""}`
+			),
+		"\n"
+	);
 };
