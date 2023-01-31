f143410ed17d7bc796ce5f7022fb397bfedfda2e
tanishqmanuja <tanishqmanuja@gmail.com>
2023-01-31 14:07:05 +0530

    feat(api): specify chat type in postChatMessage

--- api-client/src/core/chat.ts
+++ api-client/src/core/chat.ts
@@ -4,14 +4,15 @@ import type { PartyChatInfo } from "../types/chat.js";
 export async function postChatMessage(
 	this: ApiClient,
 	cid: string,
-	message: string
+	message: string,
+	type: "groupchat" | "system" = "groupchat"
 ) {
 	return this.fetch("local", `/chat/v6/messages/`, {
 		method: "POST",
 		data: {
 			cid,
 			message,
-			type: "groupchat",
+			type,
 		},
 	});
 }
