9cfbaf2ac8dfd8b549dee158f4638204542632aa
tanishqmanuja <tanishqmanuja@gmail.com>
2023-02-02 12:49:27 +0530

    feat(vry): weapon plugin support for multiple weapon flags

--- vry-js/src/plugins/player-weapons.ts
+++ vry-js/src/plugins/player-weapons.ts
@@ -24,17 +24,21 @@ export default class PlayerWeaponsPlugin
 
 	private logger = this.table.getPluginLogger(this);
 
-	private preferredWeapon = "Vandal";
+	private preferredWeapons = ["Vandal"];
 
 	constructor(table: Table, flags?: string[]) {
 		super(table, flags);
 
 		if (this.flags.length) {
-			const [prefWeapon] = this.flags;
-			if (weaponsList.includes(prefWeapon.toLowerCase() as WeaponName)) {
-				this.preferredWeapon = prefWeapon;
+			const prefWeapons = this.flags;
+			if (
+				prefWeapons.every(prefWeapon =>
+					weaponsList.includes(prefWeapon.toLowerCase() as WeaponName)
+				)
+			) {
+				this.preferredWeapons = prefWeapons;
 			} else {
-				this.logger.warn("Invalid prefered weapon in config");
+				this.logger.warn("Invalid prefered weapon/s in config");
 			}
 		}
 	}
@@ -47,50 +51,47 @@ export default class PlayerWeaponsPlugin
 			return;
 		}
 
-		const skins = preGameLoadouts?.Loadouts.map(l =>
-			this.getMappedLoadout(l)
-		).map(loudout => {
-			const skin =
-				loudout[this.preferredWeapon.toLowerCase() as WeaponName].skin;
-			const regex = new RegExp(this.preferredWeapon, "ig");
-			const name = skin.displayName.replace(regex, "").trim();
-			const colorRGB = getSkinColorFromTier(skin.contentTierUuid!);
-			if (name.toLowerCase() === "standard") {
-				return chalk.gray(name);
-			}
-			return chalk.rgb(...colorRGB)(name);
-		});
+		const cols = this.getSkinsColumnsForDisplay(preGameLoadouts.Loadouts);
 
-		if (skins) {
-			return () => this.table.addColumn(this.preferredWeapon, skins);
-		}
+		return () =>
+			cols.forEach((c, i) => this.table.addColumn(this.preferredWeapons[i], c));
 	}
 
 	async onStateInGame() {
 		const { matchLoadouts } = this.context;
 		const inGameLoadouts = matchLoadouts as CoreGameLoadouts;
 
-		const skins = inGameLoadouts?.Loadouts.map(l =>
-			this.getMappedLoadout(l.Loadout)
-		).map(loudout => {
-			const skin =
-				loudout[this.preferredWeapon.toLowerCase() as WeaponName].skin;
-			const regex = new RegExp(this.preferredWeapon, "ig");
-			const name = skin.displayName.replace(regex, "").trim();
-			const colorRGB = getSkinColorFromTier(skin.contentTierUuid!);
-			if (name.toLowerCase() === "standard") {
-				return chalk.gray(name);
-			}
-			return chalk.rgb(...colorRGB)(name);
-		});
+		const cols = this.getSkinsColumnsForDisplay(
+			inGameLoadouts?.Loadouts.map(l => l.Loadout)
+		);
 
-		if (skins) {
-			return () => this.table.addColumn(this.preferredWeapon, skins);
-		}
+		return () =>
+			cols.forEach((c, i) => this.table.addColumn(this.preferredWeapons[i], c));
 	}
 
 	private getMappedLoadout(loadout: Loadout) {
 		const { weapons } = this.essentialContent;
 		return getMappedLoadout(loadout, weapons);
+	}
+
+	private getSkinsColumnsForDisplay(loadouts: Loadout[]) {
+		return this.preferredWeapons
+			.map(prefWeapon => {
+				const skins = loadouts
+					.map(l => this.getMappedLoadout(l))
+					.map(loudout => {
+						const skin = loudout[prefWeapon.toLowerCase() as WeaponName].skin;
+						const regex = new RegExp(prefWeapon, "ig");
+						const name = skin.displayName.replace(regex, "").trim();
+						const colorRGB = getSkinColorFromTier(skin.contentTierUuid!);
+						if (name.toLowerCase() === "standard") {
+							return chalk.gray(name);
+						}
+						return chalk.rgb(...colorRGB)(name);
+					});
+
+				return skins;
+			})
+			.filter(Boolean);
 	}
 }
