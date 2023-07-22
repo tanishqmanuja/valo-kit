991b26fdfe0f6c534a52d74a19f70c0162777134
tanishqmanuja <tanishqmanuja@gmail.com>
2023-07-22 18:16:22 +0530

    feat(vry): add tdm to LUT

--- vry-js/src/formatters/queue.ts
+++ vry-js/src/formatters/queue.ts
@@ -1,19 +1,20 @@
 import { capitalizeFirstLetter } from "../utils/helpers/text.js";
 
 export const queueNameLUT: Record<string, string> = {
-	newmap: "New Map",
-	competitive: "Competitive",
-	unrated: "Unrated",
-	spikerush: "Spike Rush",
-	deathmatch: "Deathmatch",
-	ggteam: "Escalation",
-	onefa: "Replication",
-	custom: "Custom",
-	snowball: "Snowball Fight",
-	swiftplay: "Swift Play",
-	"": "Custom",
+  newmap: "New Map",
+  competitive: "Competitive",
+  unrated: "Unrated",
+  spikerush: "Spike Rush",
+  deathmatch: "Deathmatch",
+  ggteam: "Escalation",
+  onefa: "Replication",
+  custom: "Custom",
+  snowball: "Snowball Fight",
+  swiftplay: "Swift Play",
+  hurm: "Team Deathmatch",
+  "": "Custom",
 };
 
 export const getQueueName = (queueId: string) => {
-	return queueNameLUT[queueId] ?? capitalizeFirstLetter(queueId);
+  return queueNameLUT[queueId] ?? capitalizeFirstLetter(queueId);
 };
